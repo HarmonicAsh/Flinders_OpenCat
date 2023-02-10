0078ff55cbdead8890e28db574a01b3c92aba70d
HarmonicAsh <120606805+HarmonicAsh@users.noreply.github.com>
2023-02-10 14:10:02 +1030

    Audio.py
    Updated for wav files

--- OpenCat1801/serialMaster/Audio.py
+++ OpenCat1801/serialMaster/Audio.py
@@ -4,26 +4,11 @@ import time
 from pygame import mixer
 
 mixer.init()  #Initialise pigame mixer
-cat1 = mixer.Sound('cat1.mp3')   #load mp3 files
-cat2 = mixer.Sound('cat2.mp3') 
-cat3 = mixer.Sound('cat3.mp3') 
-cat4 = mixer.Sound('cat4.mp3') 
+cat2 = mixer.Sound("cat2.wav")   #load wav files
+mixer.Sound.play(cat2)
+time.sleep(1)
 
 
-try:
-    while True:
-        print("Press 1 ~ 4 to play audio")
-        command = input()
-        print("Playback beginning...")
-        if command == "1":
-            cat1.play()
-            print("Playback finished/n")
-        elif command == "2":
-            cat2.play()
-            print("Playback finished/n")
-        elif command == "3":
-            cat3.play()
-            print("Playback finished/n")
-        elif command == "4":
-            cat4.play()
-            print("Playback finished/n")
+def meow(int):
+    if int == 0:
+        mixer.Sound.play(cat2)
